9d35b0120300545c3dfd0e777e300e90caff6b0f
Lena Stotska <i.olena.stotska@gmail.com>
2018-07-02 19:56:32 +0300

    feat(articles): render articles from JSON file

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 import './css/style.scss'
+import 'whatwg-fetch'
+
+function main() {
+  return getArticles()
+    .then(renderArticles)
+    .catch(error => {
+      console.error(error)
+    })
+}
+
+function getArticles() {
+  return fetch('db.json')
+    .then(response => response.json())
+}
+
+function renderArticles(articles) {
+  const template = document.getElementById('post-container')
+  const main = document.getElementById('container')
+
+  articles.forEach(article => {
+    const articleNode = template.content.cloneNode(true)
+    main.appendChild(render(articleNode, article))
+  })
+}
+
+function render(element, obj) {
+  const elements = Array.from(element.querySelectorAll('[data-bind]'))
+
+  elements.forEach(el => {
+    const bindings = el.getAttribute('data-bind').split(',')
+
+    bindings.forEach(binding => {
+      const [field, attr] = binding.split(':')
+
+      if (attr) {
+        el.setAttribute(attr, obj[field])
+      } else {
+        el.textContent = obj[field]
+      }
+    })
+  })
+
+  return element
+}
+
+main()
+
